53db66bc8827719c60fdacbcc2f0861cce7163ba
Alex Oxthorn <zamerick@gmail.com>
2019-04-20 15:35:28 -0400

    fix wrong highlightjs link

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,11 @@
         <meta name="theme-color" content="#0c0e10">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <script charset="UTF-8" src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/go.min.js"></script>
+        <!-- Highlight.js links -->
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/styles/default.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.15.6/highlight.min.js"></script>
         <script>hljs.initHighlightingOnLoad();</script>
+
         <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
